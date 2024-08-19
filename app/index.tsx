f7d4f2120fa574668a7c1d068c19b8e5afe8af2a
Vikram <vikram.sh86@gmail.com>
2024-08-19 21:24:11 +0200

    remove unwanted files

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.text}>
+        Hello, World
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems : 'center',
+    backgroundColor : '#282c34',
+  },
+  text: {
+    color: '#61dafb',
+    fontSize: 24,
+  }
+});
